040d7ac629ab5fcdedd39a7c9133b6f628121af8
Eduardo Moguillansky <eduardo.moguillansky@gmail.com>
2019-07-02 20:33:20 +0200

    fix setup.py to be pip installable

--- setup.py
+++ setup.py
@@ -5,7 +5,6 @@ numpyx. Some accelerated funcs for numpy
 import sys
 import os
 from setuptools import setup, Extension
-import numpy
 
 
 if sys.argv[-1] == 'publish':
@@ -15,30 +14,29 @@ elif sys.argv[-1] == 'testpublish':
     os.system('python setup.py sdif upload -r pypitest')
     sys.exit()
 
-
+class numpy_get_include(str):
+    def __str__(self):
+        import numpy
+        return numpy.get_include()
+        
 setup(
     name = "numpyx",
-    setup_requires=[
-        'cython'
-    ],
+    python_requires=">=3.6",
+    setup_requires=['cython', 'numpy'],
+    install_requires = ["numpy"],
     ext_modules=[
         Extension(
             'numpyx',
             sources=['numpyx.pyx'],
-            include_dirs=[numpy.get_include()]
-        ),
+            include_dirs=[numpy_get_include()]
+        )
     ],
-    # ext_modules = cythonize('numpyx.pyx'),  # accepts a glob pattern
 
-    # metadata
-    version          = "0.3.0",
+    version          = "0.3.1",
     url              = 'https://github.com/gesellkammer/numpyx',
     download_url     = 'https://github.com/gesellkammer/numpyx',
     author           = 'eduardo moguillansky',
     author_email     = 'eduardo.moguillansky@gmail.com',
-    maintainer       = '',
-    maintainer_email = '',
-    install_requires = ["numpy", "cython"],
     description = "Utility functions for numpy, written in cython",
     license="GPL v3",
     classifiers=[
@@ -48,5 +46,6 @@ setup(
         'Natural Language :: English',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
     ]
 )
